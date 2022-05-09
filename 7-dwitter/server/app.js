9c90b6ec63a1ecbf26c819308f5e3b4139c679ca
joo717 <81240573+joo717@users.noreply.github.com>
2022-05-09 13:29:34 +0900

    ~ 12.10

--- 7-dwitter/server/app.js
+++ 7-dwitter/server/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import tweetRouter from "./router/tweet.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.send("hello 😀");
